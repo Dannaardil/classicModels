9de8542f00b44e2b6c7e027fd356dde7543f2641
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-06-14 06:20:51 -0500

    feat: :sparkles: se importaron las consultas al main

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
-import { connection } from '../../helpers/connection.js';
+import {
+    getStockProducts,
+    getTotalStockProducts,
+    getProductsMoreThan50,
+    getTotalOrderedProducts
 
-export const getAllLastName = async () => {
-    const [results] = await connection.query('SELECT lastName FROM employees');
-    return results;
-}
+    
+
+
+
+
+} from 'js/module/products.js'
+
+
+import {
+    getPaymentsFromOneClient
+
+
+
+
+} from  'js/module/payments.js'
+
+import {
+    getShippedOrders,
+    getOrdersFromOneClient
+
+
+}from 'js/module/orders.js'
+
+import {
+    getOfficesFromOneCountry,
+    getOfficesWithEmployees
+
+} from 'js/module/offices.js'
+
+import {
+    getEmployeesFromOffice,
+    getEmployeesWithBoss,
+    getEmployeesWithOffice
+
+}from 'js/module/employees.js'
+import {
+    getOneClient,
+    getClientsFromOneCity,
+    getClientsWithSalesRep
+
+} from 'js/module/costumers.js'
+console.log(await getStockProducts())
